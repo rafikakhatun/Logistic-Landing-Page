c2ef7abdcca00ed14f359b556cda5497a31d88dd
rafika khatun <rafikakhatun607@gmail.com>
2025-09-04 23:24:50 +0530

    feat: add responsive About Us section with heading and description

--- src/component/AboutUs.jsx
+++ src/component/AboutUs.jsx
@@ -9,26 +9,27 @@ const AboutUs = () => {
                 <div>
                     <p className="text-orange-500 font-semibold mb-2 text-xl">About Us</p>
 
-                    <h2 className="text-3xl font-bold leading-snug mb-4">
-                        Delivering Reliable{" "}
-                        <span className="text-orange-500">Logistics Solutions</span> Every Time
+                    <h2 className="text-2xl md:text-4xl font-bold leading-snug tracking-wide mb-4">
+                        Delivering Reliable{" "} <br />
+                        <span className="text-orange-500">Logistics Solutions Every </span><br />  Time
                     </h2>
 
-                    <p className="text-gray-600 mb-4">
+                    <p className="text-gray-600 mb-4 text-sm md:text-md">
                         At Logistip, we are more than just a transportation company.
                         We’re your trusted partner in navigating the complexities of
                         logistics and supply chain management.
                     </p>
+                    <div className="border-l-2 border-orange-500">
+                        <p className="text-gray-600 mb-6 text-sm md:text-md ml-2">
+                            25 years of experience in the industry, we have built a reputation
+                            for excellence, reliability, and customer service.
+                        </p>
+                    </div>
 
-                    <p className="text-gray-600 mb-6">
-                        25 years of experience in the industry, we have built a reputation
-                        for excellence, reliability, and customer service.
-                    </p>
 
-                   
                 </div>
 
-                
+
             </div>
         </section>
     );
