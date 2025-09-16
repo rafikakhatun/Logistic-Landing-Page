4e25f30c8da1d3eabb93c60d1492ffd01c4c6c48
rafika khatun <rafikakhatun607@gmail.com>
2025-09-16 21:43:44 +0530

    add aos animations to BlogArticle section

--- src/component/BlogArticle.jsx
+++ src/component/BlogArticle.jsx
@@ -7,64 +7,70 @@ import BlogImg3 from "../assets/blogimg3.jpeg"
 const BlogArticle = () => {
     return (
         <>
-            <section className="bg-white max-w-6xl mx-auto w-full py-16 px-10">
+            <section className="bg-white max-w-6xl mx-auto w-full py-16 px-10" data-aos="fade-up">
                 {/*heading content*/}
-                <div className="flex flex-col md:flex-row justify-between items-center md:items-end space-y-4 ">
+                <div className="flex flex-col md:flex-row justify-between items-center md:items-end space-y-4 " data-aos="fade-down">
                     {/*left section*/}
-                    <div className="flex flex-col">
-                        <h2 className="text-orange-500 font-semibold mb-2 text-xl text-center md:text-left">Blog & Article</h2>
-                        <h1 className="text-2xl md:text-4xl lg:text-4xl font-semibold text-[#242254] text-center md:text-left leading-snug">Get The <span className="text-orange-500">Latest</span> Our News</h1>
-
+                    <div className="flex flex-col" data-aos="zoom-in">
+                        <h2 className="text-orange-500 font-semibold mb-2 text-xl text-center md:text-left" data-aos="fade-up"
+                            data-aos-delay="200">Blog & Article</h2>
+                        <h1 className="text-2xl md:text-4xl lg:text-4xl font-semibold text-[#242254] text-center md:text-left leading-snug" data-aos="fade-right"
+                            data-aos-duration="1000">
+                            Get The <span className="text-orange-500">Latest</span> Our News
+                        </h1>
                     </div>
                     {/*right section*/}
-                    <div className="flex gap-3">
+                    <div className="flex gap-3" data-aos="zoom-in-left">
                         <div className="flex bg-[#242254] text-xl text-white p-2 rounded-md cursor-pointer"><GoArrowLeft /></div>
                         <div className="flex bg-[#242254] text-xl text-white p-2 rounded-md cursor-pointer"><GoArrowRight /></div>
-
                     </div>
-
                 </div>
 
                 {/*Blog section*/}
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 place-items-center mt-20">
                     {/*Blog 1*/}
-                    <div className="w-80 shadow-lg rounded-md">
+                    <div className="w-80 shadow-lg rounded-md" data-aos="flip-left">
                         <img className="w-full h-64 object-cover rounded-t-md hover:transition-transform duration-300 ease-in hover:scale-75 cursor-pointer" src={BlogImg1} alt="BlogImg1" />
                         <div className="px-4 py-6 space-y-2">
                             <p className="text-gray-600">by Admin | 10 May 2025 | Logistic</p>
-                            <h1 className="text-[#242254] text-lg md:text-xl font-bold">Optimizing Supply Chain <br className="hidden md:block" />Efficiency</h1>
-                            <button className="flex text-orange-500 font-semibold hover:underline underline-offset-2 cursor-pointer">Read More<GoArrowUpRight className="mt-1" /></button>
+                            <h1 className="text-[#242254] text-lg md:text-xl font-bold">
+                                Optimizing Supply Chain <br className="hidden md:block" />Efficiency
+                            </h1>
+                            <button className="flex text-orange-500 font-semibold hover:underline underline-offset-2 cursor-pointer" data-aos="zoom-in">
+                                Read More<GoArrowUpRight className="mt-1" />
+                            </button>
                         </div>
                     </div>
 
                     {/*Blog 2*/}
-                    <div className="w-80 shadow-lg rounded-md">
+                    <div className="w-80 shadow-lg rounded-md" data-aos="flip-up">
                         <img className="w-full h-64 object-cover rounded-t-md hover:transition-transform duration-300 ease-in hover:scale-75 cursor-pointer" src={BlogImg2} alt="BlogImg1" />
                         <div className="px-4 py-6 space-y-2">
                             <p className="text-gray-600">by Admin | 24 Feb 2025 | Transport</p>
-                            <h1 className="text-[#242254] text-lg md:text-xl font-bold">navigating <br className="hidden md:block" />International customer</h1>
-                            <button className="flex text-orange-500 font-semibold hover:underline underline-offset-2 cursor-pointer">Read More<GoArrowUpRight className="mt-1" /></button>
+                            <h1 className="text-[#242254] text-lg md:text-xl font-bold">
+                                navigating <br className="hidden md:block" />International customer
+                            </h1>
+                            <button className="flex text-orange-500 font-semibold hover:underline underline-offset-2 cursor-pointer" data-aos="zoom-in">
+                                Read More<GoArrowUpRight className="mt-1" />
+                            </button>
                         </div>
                     </div>
 
-
                     {/*Blog 3*/}
-                    <div className="w-80 shadow-lg rounded-md">
+                    <div className="w-80 shadow-lg rounded-md" data-aos="flip-right">
                         <img className="w-full h-64 object-cover rounded-t-md hover:transition-transform duration-300 ease-in hover:scale-75 cursor-pointer" src={BlogImg3} alt="BlogImg1" />
                         <div className="px-4 py-6 space-y-2">
                             <p className="text-gray-600">by Admin | 10 Jan 2025 | Warehouse</p>
-                            <h1 className="text-[#242254] text-lg md:text-xl font-bold">Sustainable Logistic Building <br className="hidden md:block" /> a Greener</h1>
-                            <button className="flex text-orange-500 font-semibold hover:underline underline-offset-2 cursor-pointer">Read More<GoArrowUpRight className="mt-1" /></button>
+                            <h1 className="text-[#242254] text-lg md:text-xl font-bold">
+                                Sustainable Logistic Building <br className="hidden md:block" /> a Greener
+                            </h1>
+                            <button className="flex text-orange-500 font-semibold hover:underline underline-offset-2 cursor-pointer" data-aos="zoom-in">
+                                Read More<GoArrowUpRight className="mt-1" />
+                            </button>
                         </div>
                     </div>
-
                 </div>
-
-
-
             </section>
-
-
         </>
     );
 };
