56ebf0b26f3357f57239e34fbf1596e39ef2f514
rafika khatun <rafikakhatun607@gmail.com>
2025-09-16 20:44:12 +0530

    import all images

--- src/component/BlogArticle.jsx
+++ src/component/BlogArticle.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import BlogImg1 from "../assets/blogimg1.jpeg"
+import BlogImg2 from "../assets/blogimg2.jpeg"
+import BlogImg3 from "../assets/blogimg3.jpeg"
+
 
 const BlogArticle = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+
+        </>
     );
 };
 
