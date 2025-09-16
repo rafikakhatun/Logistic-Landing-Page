35d493d2661d824b5ab54af6d479246d3e1505d9
rafika khatun <rafikakhatun607@gmail.com>
2025-09-16 19:20:12 +0530

    import all logo in ourPartner section

--- src/component/OurPartner.jsx
+++ src/component/OurPartner.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import GigaByteLogo from "../assets/gigabyteLogo.avif"
+import TexasLogo from "../assets/TexasInstruments-Logo.svg.png"
+import TangemLogo from "../assets/tangemLogo.png"
+import SfExpressLogo from "../assets/sfExpress.png"
+import AmazonLogo from "../assets/Amazon-Logo.png"
+import GopassLogo from "../assets/goPassLogo.png"
 
 const OurPartner = () => {
     return (
-        <div>
-            
+       <section>
+        {/*heading section*/}
+        <h2>Our Partner</h2>
+        <h1>Our Trusted Collaaborators</h1>
+
+        {/*Logo ssection*/}
+        <div className="max-w-6xl mx-auto ">
+            {/* Logo 1 */}
+            <div>
+                <img src={GigaByteLogo} alt="Logo" />
+            </div>
+
+            {/* Logo 2 */}
+            <div>
+                <img src={TexasLogo} alt="Logo" />
+            </div>
+
+            {/* Logo 3 */}
+            <div>
+                <img src={TangemLogo} alt="Logo" />
+            </div>
+
+            {/* Logo 4 */}
+            <div>
+                <img src={SfExpressLogo} alt="Logo" />
+            </div>
+
+            {/* Logo 5 */}
+            <div>
+                <img src={AmazonLogo} alt="Logo" />
+            </div>
+
+            {/* Logo 6 */}
+            <div>
+                <img src={GopassLogo} alt="Logo" />
+            </div>
         </div>
+        
+       </section>
     );
 };
 
